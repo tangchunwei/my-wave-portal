b9bc6ed3775711adfdfbe3acdfc1953477a54700
danding <dandingkeji.top@gmail.com>
2022-11-09 23:00:00 +0800

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+// Import and configure dotenv
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+      console.log(account.address);
+  }
+});
+
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
